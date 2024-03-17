ef45173c98d1ebefea4eea26d222d953e0c0a86f
Ellen Xu <ellen2imagine@gmail.com>
2024-03-16 17:58:57 -0700

    .

--- brain.c
+++ brain.c
@@ -1,15 +1,14 @@
-#include "bt_ext.h"
-#include "gpio.h"
 #include "interrupts.h"
 #include "jnxu.h"
 #include "printf.h"
-#include "re.h"
 #include "timer.h"
 #include "uart.h"
-#include <stdint.h>
 #include "chess.h"
 #include "chess_gui.h"
-#include "gl.h"
+#include <stddef.h>
+
+#define BT_MODE BT_EXT_ROLE_SUBORDINATE
+#define BT_MAC  NULL
 
 void brain(void) {
     interrupts_init();
@@ -20,7 +19,10 @@ void brain(void) {
 
     chess_gui_print();
     timer_delay(5);
-    chess_gui_update("e8g8\n");
+
+    jnxu_init(BT_MODE, BT_MAC);
+    // chess_gui_update("e8g8\n");
 
     while (1) {}
+
 }
